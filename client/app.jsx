0b5d964c1cce7f6036da4f3cd0d994b1659a49a7
adam-sungmin-park <parksm2@uci.edu>
2021-04-21 02:27:24 +0000

    navbar/bootstrap added

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Home from './pages/home';
+import NavBar from './pages/navbar';
 
 export default class App extends React.Component {
+
+  renderPage(){
+
+  }
+
+
+
+
+
+
   render() {
-    return <Home />;
+    return (
+      <>
+      <NavBar/>;
+      <Home />;
+      </>
+
+    )
   }
 }
---
+++ client/pages/navbar.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { render } from 'react-dom';
+export default class NavBar extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state= {
+      userId: this.props.userId,
+      view:"no"
+    }
+    this.handleSignOut = this.handleSignOut.bind(this);
+    this.handleReload = this.handleReload.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleReload(){
+    window.location.reload()
+  }
+
+  componentDidMount(){
+    if(this.state.userId !==null){
+      this.setState({
+        view:"yes"
+      })
+    }
+  }
+
+  handleSignOut(){
+    window.localStorage.removeItem("token")
+    this.setState({
+      userId: 0,
+      view:"no"
+    })
+    window.location.reload();
+  }
+
+  handleClick() {
+    document.querySelector('.navbar-collapse').className = 'navbar-collapse collapse'
+  }
+
+  render(){
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Demo</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="#workout" onClick = {this.handleClick}>Workout</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#food" onClick={this.handleClick}>Food</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#journal" onClick={this.handleClick}>Body Weight</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}

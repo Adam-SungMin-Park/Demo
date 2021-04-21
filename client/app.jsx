0dba345e1aa2f0c9044f918c84859ae9cd76ff2b
adam-sungmin-park <parksm2@uci.edu>
2021-04-21 04:33:30 +0000

    check

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import Home from './pages/home';
 import NavBar from './pages/navbar';
+import parseRoute from './pages/parseroute';
 
 export default class App extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      route:parseRoute(window.location.hash)
+    }
+  }
+  componentDidMount(){
+    window.addEventListener('hashchange',(event)=>{
+      this.setState({
+        route:parseRoute(window.location.hash)
+      })
+    })
+  }
   renderPage(){
 
   }

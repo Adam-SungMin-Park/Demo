489bfe09e11c45b48d17555a8966a07f83415958
adam-sungmin-park <parksm2@uci.edu>
2021-04-22 01:43:20 +0000

    add item entry

--- client/pages/additem.jsx
+++ client/pages/additem.jsx
@@ -12,7 +12,28 @@ export default class AddItem extends React.Component{
   }
   render(){
     return(
+      <>
       <h1>Add Item Page</h1>
+      <form>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">Item Name</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Item Image</label>
+          <input type="file" className="form-control" id="exampleInputPassword1"/>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Item Price</label>
+          <input type="number" min = "0" className="form-control" id="exampleInputPassword1"/>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Item Qty</label>
+          <input type="Number" min = "0" className="form-control" id="exampleInputPassword1"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+      </>
     )
   }
 }

14d12d367e5c44e706510b9f9815a3d475dbd558
adam-sungmin-park <parksm2@uci.edu>
2021-04-23 02:35:59 +0000

    database

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,13 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+CREATE TABLE "items" (
+	"itemId" serial NOT NULL,
+	"itemName" VARCHAR(255) NOT NULL,
+	"itemPrice" integer NOT NULL,
+	"itemQty" integer NOT NULL,
+	"itemImage" VARCHAR(255) NOT NULL,
+	CONSTRAINT "items_pk" PRIMARY KEY ("itemId")
+) WITH (
+  OIDS=FALSE
+);
--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
-
+const argon2  = require('argon2');
+const jsonMiddleware = express.json();
+const jwt = require('jsonwebtoken');
 const app = express();
+const pg = require('pg');
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.use(jsonMiddleware);
+
+app.post('/demo/item',(req,res)=>{
+
+})
+
+
+
 
 app.use(staticMiddleware);
 
